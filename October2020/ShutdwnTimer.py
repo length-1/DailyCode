81e42ce16f41101c088d42f172d446a460dab2b6
length-1 <43411623+length-1@users.noreply.github.com>
2020-10-10 07:35:22 -0700

    Create ShutdwnTimer.py

---
+++ October2020/ShutdwnTimer.py
@@ -0,0 +1,21 @@
+import time
+import os
+mins = 0
+t= input("enter the time in minutes")
+
+while int(mins) !=int(t):
+
+     time.sleep(10)
+     
+     mins = mins+1
+     
+
+os.system("shutdown -l") #this code will just logoff to shutdown use this instead os.system("shutdown /s /t 1")
+
+
+
+
+
+
+     
+     
