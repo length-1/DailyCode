59c503f2ea45426d86195ae1efdc6d78012a8b0b
length-1 <43411623+length-1@users.noreply.github.com>
2020-10-11 02:34:04 -0700

    Update Screenshot.py

--- October2020/Screenshot.py
+++ October2020/Screenshot.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np #this program allows to take screenshot evevry 30 seconds for 2 mins
 import cv2
 import pyautogui
 from datetime import datetime
@@ -13,13 +13,13 @@ tempMins = 0
 while tempMins != mins :
     
     title=str(datetime.now())
-    x = title.replace(":","-")#certain charecters are not allowed to file name
+    x = title.replace(":","-")#certain charecters are not allowed in file names
 
     image = cv2.cvtColor(np.array(image),
                      cv2.COLOR_RGB2BGR)
     cv2.imwrite(x + ".png", image)
 
-    time.sleep(60) #takes screen shot for every 30 seconds
+    time.sleep(30) #takes screen shot for every 30 seconds
     tempMins +=1
 print("over")
 
